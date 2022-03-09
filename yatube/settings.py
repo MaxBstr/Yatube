acbe964a126ce8652bdbfb4a7679b248217298e5
Максим Бастрыкин <maksimbastrikin@bk.ru>
2022-03-09 03:12:34 +0300

    test

--- yatube/settings.py
+++ yatube/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '9ea2we(2(i3d+r4u%l+w%^vr5*z$i6ttq5kkeqn27j3c257pq!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "itmo-web-proj.herokuapp.com",
@@ -135,7 +135,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
